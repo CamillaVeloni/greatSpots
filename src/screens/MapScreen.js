66980894249c43ebabef91772a0943c75fdd9bd8
CamillaVeloni <Zz01594120>
2021-10-01 01:48:13 -0300

    Criando tela SpotDetailsScreen e logica

--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -1,5 +1,11 @@
-import React, { useState, useLayoutEffect, useCallback } from 'react';
+import React, {
+  useState,
+  useLayoutEffect,
+  useCallback,
+  useEffect,
+} from 'react';
 import {
+  Alert,
   Dimensions,
   Platform,
   StyleSheet,
@@ -8,21 +14,57 @@ import {
   View,
 } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
+import * as Location from 'expo-location';
 
 import Colors from '../../constants/Colors';
 
-const MapScreen = ({ navigation }) => {
-  const [selectedLocation, setSelectedLocation] = useState();
+const MapScreen = ({ navigation, route }) => {
+  // Veio de SpotDetailsScreen (i.e. onClickMapHandler)
+  // readonly é usado em dois lugares: em selectLocationHandler para não ser selecionado outro lugar
+  // em useLayoutEffect para não ser criado o botão de salvar
+  const initialLocation = route.params?.initialLocation;
+  const readonly = route.params?.readonly;
 
-  const mapRegion = {
-    latitude: 37.78,
-    longitude: -122.43,
+  // State para seleção do local
+  const [selectedLocation, setSelectedLocation] = useState(initialLocation);
+  const [mapRegion, setMapRegion] = useState(initialLocation ? {
+    latitude: initialLocation.latitude,
+    longitude: initialLocation.longitude,
     latitudeDelta: 0.0922,
     longitudeDelta: 0.0421,
-  };
+  } : null);
+
+  // Verificando localização do usuário para initialRegion de novo spot
+  useEffect(() => {
+    if (readonly) return;
+
+    (async () => {
+      const { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        Alert.alert(
+          'Permissão negada',
+          'Você precisa dar acesso a localização do seu disposito para que possamos mostrar sua localização.',
+          [{ text: 'Okay' }]
+        );
+        return;
+      }
+
+      const userLocation = await Location.getCurrentPositionAsync({
+        accuracy: Location.Accuracy.Lowest,
+      });
+
+      setMapRegion({
+        latitude: userLocation.coords.latitude,
+        longitude: userLocation.coords.longitude,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      });
+    })();
+  }, [readonly]);
 
   // Pegando coordenadas com o MapView
   const selectLocationHandler = (event) => {
+    if (readonly) return;
     //console.log(event.nativeEvent);
     setSelectedLocation({
       latitude: event.nativeEvent.coordinate.latitude,
@@ -43,9 +85,11 @@ const MapScreen = ({ navigation }) => {
     });
   }, [selectedLocation]);
 
-  // A função do useLayoutEffect é 'disparado' sincronicamente, ANTES do das mutações do DOM serem feitas
+  // A função do useLayoutEffect é 'disparado' sincronicamente, ANTES das mutações do DOM serem feitas
   // Criando option de salvar no header (i.e. https://reactnavigation.org/docs/header-buttons/)
   useLayoutEffect(() => {
+    if (readonly) return;
+
     navigation.setOptions({
       headerRight: () => (
         <TouchableOpacity
@@ -56,13 +100,13 @@ const MapScreen = ({ navigation }) => {
         </TouchableOpacity>
       ),
     });
-  }, [navigation, savePickedLocationHandler]);
+  }, [navigation, readonly, savePickedLocationHandler]);
 
   return (
     <View style={styles.container}>
       <MapView
         style={styles.map}
-        region={mapRegion}
+        initialRegion={mapRegion}
         onPress={selectLocationHandler}
       >
         {selectedLocation && (
--- src/screens/SpotDetailScreen.js
+++ src/screens/SpotDetailScreen.js
@@ -1,26 +1,84 @@
 import React, { useState } from 'react';
-import { ScrollView, View, Text, StyleSheet } from 'react-native';
+import { ScrollView, View, Image, Text, StyleSheet } from 'react-native';
 import { useSelector } from 'react-redux';
+import Colors from '../../constants/Colors';
 
 import MapPreview from '../components/commons/MapPreview';
 
-const SpotDetailScreen = ({ route }) => {
+const SpotDetailScreen = ({ navigation, route }) => {
   const { spotId } = route.params;
   const spotDetails = useSelector(({ spots }) =>
     spots.spots.find((spot) => spot.id === spotId)
   );
+  const locationDetails = {
+    latitude: spotDetails.lat,
+    longitude: spotDetails.lng,
+  };
+
+  const onClickMapHandler = () => {
+    navigation.navigate('Map', {
+      readonly: true,
+      initialLocation: locationDetails,
+    });
+  };
 
   return (
-    <ScrollView>
-      <Image />
-      <View>
-        <Text>{spotDetails.address}</Text>
-        <MapPreview />
+    <ScrollView
+      contentContainerStyle={{ alignItems: 'center' }}
+      showsVerticalScrollIndicator={false}
+    >
+      <Image source={{ uri: spotDetails.photoUri }} style={styles.photo} />
+      <View style={styles.detailsContainer}>
+        <View style={styles.addressContainer}>
+          <Text style={styles.addressText}>{spotDetails.address}</Text>
+        </View>
+        <MapPreview
+          style={styles.mapPreview}
+          location={locationDetails}
+          onPress={onClickMapHandler}
+        />
       </View>
     </ScrollView>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  photo: {
+    height: '35%',
+    minHeight: 300,
+    width: '100%',
+    backgroundColor: '#ccc',
+  },
+  detailsContainer: {
+    marginVertical: 20,
+    width: '90%',
+    maxWidth: 350,
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowColor: 'black',
+    shadowOpacity: 0.26,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
+    elevation: 5,
+    backgroundColor: 'white',
+    borderRadius: 10,
+  },
+  addressContainer: {
+    padding: 20,
+  },
+  addressText: {
+    fontFamily: 'nunito-regular',
+    fontSize: 16,
+    color: Colors.primary,
+    textAlign: 'center',
+  },
+  mapPreview: {
+    width: '100%',
+    maxWidth: 350,
+    height: 300,
+    borderBottomLeftRadius: 10,
+    borderBottomRightRadius: 10,
+  },
+});
 
 export default SpotDetailScreen;
